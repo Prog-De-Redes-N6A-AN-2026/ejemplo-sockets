0aac9d6ee31df0e403ed7792cc2c359a8215ae30
0x5abe <sabemasterprox@gmail.com>
2026-09-03 20:45:19 -0300

    Ejemplo inicial

---
+++ Client/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Cliente
+{
+    internal class Cliente
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Empezando cliente!");
+
+            Socket socketCliente = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            
+            IPEndPoint endpointLocal = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0);
+            socketCliente.Bind(endpointLocal);
+            
+            IPEndPoint endpointServidor = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            socketCliente.Connect(endpointServidor);
+            Console.WriteLine("Conectado al servidor!");
+
+            bool salir = false;
+            while (!salir)
+            {
+                Console.Write("Ingresar un mensaje para el servidor:");
+                string mensaje = Console.ReadLine();
+                if (mensaje == "salir")
+                {
+                    salir = true;
+                    continue;
+                }
+                byte[] bufferMensaje = Encoding.UTF8.GetBytes(mensaje);
+                int largoMensaje = bufferMensaje.Length;
+                byte[] bufferLargoMensaje = BitConverter.GetBytes(largoMensaje);
+                socketCliente.Send(bufferLargoMensaje);
+                socketCliente.Send(bufferMensaje);
+                Console.WriteLine($"Mensaje enviado: \"{mensaje}\"");
+            }
+            Console.WriteLine("Se cierra la conexion...");
+            socketCliente.Shutdown(SocketShutdown.Both); // Indica en que sentido quiero cerrar la conexion
+            socketCliente.Close();
+        }
+    }
+}
---
+++ Server/Servidor.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Servidor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Empezando servidor!");
+
+            Socket socketServer = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            
+            IPEndPoint endpointLocal = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000); 
+            
+            socketServer.Bind(endpointLocal);
+
+            socketServer.Listen(10); // escuchamos conexiones
+
+            Console.WriteLine("Esperando a que se conecten clientes...");
+            int clientesConectados = 0;
+            while (true)
+            {
+                Socket socketCliente = socketServer.Accept(); // bloqueante
+                clientesConectados++;
+                int clienteId = clientesConectados;
+                Thread t = new Thread(() => ManejarCliente(socketCliente, clienteId)); 
+                t.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ManejarCliente(Socket socketCliente, int clienteId)
+        {
+            Console.WriteLine($"Se conectó el cliente #{clienteId}!");
+            bool clienteConectado = true;
+            do
+            {
+                int recibidos;
+                byte[] bufferLargoMensaje = new byte[sizeof(int)];
+                recibidos = socketCliente.Receive(bufferLargoMensaje); // bloqueante
+                if (recibidos == 0)
+                {
+                    clienteConectado = false;
+                    continue;
+                }
+                int largoMensaje = BitConverter.ToInt32(bufferLargoMensaje);
+                byte[] buffer = new byte[largoMensaje];
+                recibidos = socketCliente.Receive(buffer); // bloqueante
+                if (recibidos == 0)
+                {
+                    clienteConectado = false;
+                    continue;
+                }
+                string mensaje = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"El cliente #{clienteId} envió: {mensaje}");
+            } while (clienteConectado);
+            Console.WriteLine($"Se cerro la conexion del lado del cliente #{clienteId}");
+        }
+    }
+}
